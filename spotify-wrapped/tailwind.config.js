cb5972c3f0c0ada545b018fca8d6913c8e6dd0d8
TomBarlow1 <150055424+TomBarlow1@users.noreply.github.com>
2024-12-26 12:23:53 +0000

    small update

--- spotify-wrapped/tailwind.config.js
+++ spotify-wrapped/tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Your files where Tailwind classes will be used
+  ],
   theme: {
     extend: {},
   },
